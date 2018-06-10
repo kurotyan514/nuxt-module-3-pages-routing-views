05329594aa53d63dab2fb6d8922749196abe54ad
= <kurotyan514@gmail.com>
2018-06-10 17:44:31 +0800

    add global font family
    use google font to add global font family

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,9 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // load font from google font on every page
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Indie+Flower" }
     ]
   },
 
